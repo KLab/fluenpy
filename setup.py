19ff2c27633b857a5c3fd7d00b8e4b5ca2e70eb1
INADA Naoki <inada-n@klab.jp>
2012-04-30 20:08:55 +0900

    Add fluenpy.__version__

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 from distutils.core import setup
+exec(open('fluenpy/version.py').read())
 
 setup(name="fluenpy",
-      version="0.0.1dev",
+      version=__version__,
       packages=['fluenpy', 'fluenpy.plugins'],
       scripts=['scripts/fluen.py'],
       )
