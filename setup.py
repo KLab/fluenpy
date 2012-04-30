afcf73bbccfb235c4428d1cffa667ef12c836fd5
INADA Naoki <inada-n@klab.jp>
2012-04-30 12:07:04 +0900

    Add setup script and requirements.

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name="fluenpy",
+      version="0.0.1dev",
+      packages=['fluenpy', 'fluenpy.plugins'],
+      scripts=['scripts/fluen.py'],
+      )
