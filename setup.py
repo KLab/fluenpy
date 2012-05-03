33945e7b83c0e6d51bc88aad1845bb3c8434ad75
INADA Naoki <inada-n@klab.jp>
2012-05-04 02:04:32 +0900

    Use setuptools.setup if available.

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# load __version__ without importing any other files.
 exec(open('fluenpy/version.py').read())
 
 setup(name="fluenpy",
       version=__version__,
-      packages=['fluenpy', 'fluenpy.plugins'],
+      packages=['fluenpy',
+                'fluenpy.plugins'
+                ],
       scripts=['scripts/fluen.py'],
       )
